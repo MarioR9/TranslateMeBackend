918ec31ee2e1f44775fc0d2f5f8cec3d9efebcfd
Marior9 <mariorodriguezan@gmail.com>
2019-06-28 23:12:57 -0400

    testing visual recognition api by fetching a post request from the frotend

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,31 @@
+
+require 'tempfile'
+
+        require "json"
+        require "ibm_watson/visual_recognition_v3"
+        require "ibm_watson/language_translator_v3"
+
+        include IBMWatson
+
 class Api::V1::UsersController < ApplicationController
 
     def index 
+        
         render json: User.all, include: '**'
     end 
+    def create 
+        byebug
 
+        visual_recognition = VisualRecognitionV3.new(
+        version: "2018-03-19",
+        iam_apikey: "zuJHfpYtm9gFQZDMLufjdFbWC8FSg121CVLVO9ZWYMRp"
+        )
+
+        File.open(params[:files]) do |images_file|
+            classes = visual_recognition.classify(
+            images_file: images_file
+            )    
+        render json: { result: classes.result}
+        end
+    end
 end

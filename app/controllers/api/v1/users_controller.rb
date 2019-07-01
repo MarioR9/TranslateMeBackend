40f02fca07535b7f76c0f588c35d2998d87e3a04
Marior9 <mariorodriguezan@gmail.com>
2019-07-01 08:46:47 -0400

    bugs fixed

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,10 @@
 
-# require 'tempfile'
-require_relative '../inventory.rb'
+require_relative '../../../../inventory.rb'
 
-        require "json"
-        require "ibm_watson/visual_recognition_v3"
-        require "ibm_watson/language_translator_v3"
-
-        include IBMWatson
+require "json"
+require "ibm_watson/visual_recognition_v3"
+require "ibm_watson/language_translator_v3"
+include IBMWatson
 
 class Api::V1::UsersController < ApplicationController
 
@@ -15,7 +13,7 @@ class Api::V1::UsersController < ApplicationController
         render json: User.all, include: '**'
     end 
     def create 
-        byebug
+        # byebug
 
         visual_recognition = VisualRecognitionV3.new(
         version: VISUAL_VERSION,
@@ -26,7 +24,7 @@ class Api::V1::UsersController < ApplicationController
             classes = visual_recognition.classify(
             images_file: images_file
             )    
-        render json: { result: classes.result}
+        render json: { result: classes.result , img: params[:files]}
         end
     end
 end

6abe05de621d96808c97b10e32c315f950a03bba
MarioR9 <mariorodriguezan@gmail.com>
2019-07-04 00:46:42 -0400

    refactor controllers to make sure api fetch is done propertly

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -11,26 +11,55 @@ class Api::V1::CategoriesController < ApplicationController
     end
     
     def visualRecognition 
-
+      
         visual_recognition = VisualRecognitionV3.new(
             version: VISUAL_VERSION,
             iam_apikey: VISUAL_KEY
           )
           url= params[:imgUrl]
           classes = visual_recognition.classify({url: url})
+          result = classes.result
          
-          render json: { result: classes.result}
+          @hashOfResults = result.first[1][0].first[1].first["classes"].select { |hash| hash["score"] >=  0.9 }
+          @arryOfResults = @hashOfResults.map {|result| result["class"]}
+        if params[:oglanguage] == "en"
+            render json: {result: classes.result, arrOfRes: @arryOfResults}
+        else
+            arr=[]
+            i = 0
+            while i < @arryOfResults.length do
+            language_translator = LanguageTranslatorV3.new(
+                version: TRANSLATE_VERSION,
+                iam_apikey: TRANSLATE_KEY  
+                )
+                
+            translation = language_translator.translate(
+                text: @arryOfResults[i],
+                model_id: "en-#{params[:oglanguage]}"
+                )
+                arr.push(translation.result)
+                i += 1
+            end
+            arr
+            
+            render json: {result: classes.result, arrOfRes: @arryOfResults ,translation: arr}
+         end        
     end
+
+
     def translate
-          language_translator = LanguageTranslatorV3.new(
-          version: TRANSLATE_VERSION,
-          iam_apikey: TRANSLATE_KEY  
-          )
+        byebug
+        language_translator = LanguageTranslatorV3.new(
+            version: TRANSLATE_VERSION,
+            iam_apikey: TRANSLATE_KEY  
+            )
+        translation = language_translator.translate(
+             text: params[:selectedWord],
+             model_id: "#{params[:oglanguage]}-#{params[:tglanguage]}"
+             )
+             render json: {translation: translation.result}
           
-          translation = language_translator.translate(
-          text: "#{params[]}",
-          model_id: "en-#{params[:oglanguage]}"
-          )
-          puts JSON.pretty_generate(translation.result)
+       
     end
+
 end

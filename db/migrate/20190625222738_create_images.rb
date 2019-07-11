6774dfaf8d6bb62c6103b5d540183f6890e4a821
Marior9 <mariorodriguezan@gmail.com>
2019-07-11 10:32:38 -0400

    added new style

--- db/migrate/20190625222738_create_images.rb
+++ db/migrate/20190625222738_create_images.rb
@@ -2,6 +2,8 @@ class CreateImages < ActiveRecord::Migration[6.0]
   def change
     create_table :images do |t|
       t.string :input
+      t.string :orglanguage
+      t.string :original
       t.string :tarlanguage
       t.integer :category_id
       t.string :url
